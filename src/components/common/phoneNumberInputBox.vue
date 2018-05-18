3728f487f75209e56b3014a6547b915032947fb2
sissiyam <yamlou23@gmail.com>
2018-05-18 17:29:40 +0800

    pre test login strcture

---
+++ src/components/common/phoneNumberInputBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="input_text">
+    <el-input v-model="value" placeholder="手机号码" clearable maxlength="11">
+      <template slot="prepend">
+        <div style="width: 35px;">
+          <span style="color: black;">+86</span>
+        </div>
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "phoneNumberInputBox"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--<header id="head_top">-->
+    <!--<slot name='logo'></slot>-->
+    <!--<slot name='search'></slot>-->
+    <!--<section class="head_goBack" v-if="goBack" @click="$router.go(-1)">-->
+      <!--//icon or img or somethings else to click-->
+      <!--<img class="goBackIcon" src="../../src/images/icon_back.png"/>-->
+    <!--</section>-->
+
+
+  <!--</header>-->
+</template>
+
+<script>
+    export default {
+        name: "header.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
