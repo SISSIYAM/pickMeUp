6a7c4a537b9712b33249b5e2c01aff2a3861fc31
sissiyam <yamlou23@gmail.com>
2018-05-25 17:24:01 +0800

    add store dir

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import store from './store'
 
 import './images'
 
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
